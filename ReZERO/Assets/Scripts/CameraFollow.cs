b82157308668d581a61a30cf783b7a10640f8e16
Tam Lu <tamlu907@gmail.com>
2016-08-30 21:37:16 -0700

    Walkable character

---
+++ ReZERO/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float cam_speed = 0.1f;
+	Camera camera;
+
+	// Use this for initialization
+	void Start () {
+
+		camera = GetComponent<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camera.orthographicSize = (Screen.height / 100f) / 4f; //4f is arbituary. crossplatform
+
+		if (target) {
+			transform.position = Vector3.Lerp(transform.position, target.position, cam_speed) + new Vector3(0, 0, -10);
+		}
+	}
+}
